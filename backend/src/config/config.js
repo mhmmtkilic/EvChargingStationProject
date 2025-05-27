97fc635eb7e60064cca8db58b62084588193c1d4
Muhammet Ali KILIÇ <mhmmtkilicc@gmail.com>
2025-05-28 00:23:40 +0300

    Update config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   port: process.env.PORT || 3000,
   tomtomApi: {
-    apiKey: process.env.TOMTOM_API_KEY || 'lfgX3eS62AOuWxr0GeUPQZw5CEHlA9In',
+    apiKey: process.env.TOMTOM_API_KEY || 'api key',
     baseUrl: 'https://api.tomtom.com'
   }
 }; 
